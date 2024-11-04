e9bb3b73b8e8695d2ac1a698811ce4fbfd2db9eb
ritz2405 <ruturajpatil782@gmail.com>
2024-11-05 00:16:41 +0530

    Updated

--- XRay/cloud_storage/s3_ops.py
+++ XRay/cloud_storage/s3_ops.py
@@ -3,8 +3,25 @@ import sys
 sys.path.append('D:\\Full\\Pnemonia_proj')
 from XRay.exception import XRayException
 
-def get_data_from_s3(self):
-    try:
-        pass
-    except Exception as e:
-        raise XRayException(e, sys) 
+class S3Operation:
+    def sync_folder_to_s3(
+            self, folder: str, bucket_name: str, bucket_folder_name: str
+    )-> None:
+        try: 
+            command: str = (
+                f"aws s3 sync {folder} s3://{bucket_name}/{bucket_folder_name}/"
+            )
+            os.system(command)
+        except Exception as e:
+            raise XRayException(e, sys)
+        
+    def sync_folder_from_s3(
+            self, folder: str, bucket_name: str, bucket_folder_name: str
+    )-> None:
+        try:
+            command: str = (
+                f"aws s3 sync s3://{bucket_name}/{bucket_folder_name}/{folder}"
+            )
+            os.system(command)
+        except Exception as e:
+            raise XRayException(e,sys)
